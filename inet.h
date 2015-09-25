473de5dcd938c9d736b40ecbee1196b7f61a9bdf
BeginnerZero <BeginnerZero@users.noreply.github.com>
2015-09-25 16:42:37 +0800

    Create inet.h

---
+++ inet.h
@@ -0,0 +1,27 @@
+/*	inet.h file
+	
+	TTP3121 TCP/IP Programming Project (Trimester 1, 2015/16) 
+	Members: 1) H'ng Jaan Lin	1122701742
+		 2) Lim Zhen Yang	1122701265 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define SERV_TCP_PORT 25000
+#define SERV_UDP_PORT 35001
+#define CLI_UDP_PORT 35002
+
+
+
+
+
+
+
